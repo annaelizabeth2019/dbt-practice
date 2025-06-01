06a5601e37fca55516bef67a99c0acfc88f1052d
annapeterson89@gmail.com <annapeterson89@gmail.com>
2025-06-01 15:56:13 -0400

    creates a model

---
+++ models/example/stg_customers.sql
@@ -0,0 +1,9 @@
+WITH stg_customers AS (
+    SELECT
+        id as customer_id,
+        CONCAT(first_name, ' ', last_name) as customer_name,
+        email as email_address,
+        FROM test.customers
+)
+
+SELECT * from stg_customers
