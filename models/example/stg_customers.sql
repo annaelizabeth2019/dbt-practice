d257a737ad3dcde44bf825f87975c484aa49ea66
Anna Peterson <annapeterson89@gmail.com>
2025-06-01 20:02:05 +0000

    commit 2

--- models/example/stg_customers.sql
+++ models/example/stg_customers.sql
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-With stg_customers AS (
-    SELECT 
-        id,
-        CONCAT(first_name, ' ', last_name) as customer_name,
-        email as email_address
-        FROM test.customers
-)
-
-selct * from stg_customers
-=======
-WITH stg_customers AS (
+With  stg_customers AS (
     SELECT
         id as customer_id,
         CONCAT(first_name, ' ', last_name) as customer_name,
@@ -18,4 +7,3 @@ WITH stg_customers AS (
 )
 
 SELECT * from stg_customers
->>>>>>> 06a5601e37fca55516bef67a99c0acfc88f1052d
